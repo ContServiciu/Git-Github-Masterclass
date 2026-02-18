99223d70ee157e499fdbe73f4fec1101cbb0ce75
MaFomedanu <constantinliviudaniel@yahoo.com>
2026-02-18 17:21:54 +0200

    added files to work on for section 6 of the course

--- Section 06 - Collaboration in Github/readme.md
+++ Section 06 - Collaboration in Github/readme.md
@@ -1,1 +1,3 @@
 For this particular section a different repository was used. Changes may be cheeckd out [here](https://github.com/ConstantinLiviu/TextAnalyticsGitGitHubMasterclassCourse.git)
+
+Redid section to include repos I am able to modify
---
+++ Section 06 - Collaboration in Github/script.js
@@ -0,0 +1,20 @@
+alert("This is an alert!");
+
+const inputEl = document.getElementById("celsiusInput");
+const rezultatEl = document.getElementById("rezultat");
+
+function convertCelsiusToFahrenheit(celsius) {
+  return (celsius * 9) / 5 + 32;
+}
+
+inputEl.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    let userInput = inputEl.value;
+    if (userInput === "" || isNaN(userInput)) {
+      alert("Please input a valid integer!");
+      inputEl.value = "";
+      return;
+    }
+    rezultatEl.innerText = `Temperature is ${convertCelsiusToFahrenheit(userInput)} °F`;
+  }
+});
